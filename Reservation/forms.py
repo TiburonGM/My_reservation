ad417aa86a50aa7a23a29b1f97ca12965bd7b4d3
TiburonGM <liban2gomez@mail.com>
2024-11-24 19:00:14 -0500

    new

--- Reservation/forms.py
+++ Reservation/forms.py
@@ -5,4 +5,9 @@ class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reserva
         fields = ['mesa', 'cliente', 'fecha', 'hora', 'duracion', 'comentario']
-        
+        widgets = {
+            'fecha': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'hora': forms.TimeInput(attrs={'class': 'form-control', 'type': 'time'}),
+            'mesa': forms.Select(attrs={'class': 'form-select'}),
+            'cliente': forms.Select(attrs={'class': 'form-select'}),
+        }
